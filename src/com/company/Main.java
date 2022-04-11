666ba2656a2304c6fe8fac2cad1901a2786f974d
Tatiana Terentyeva <tterentieva17@gmail.com>
2022-04-11 12:55:01 +0300

    new line22

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello, QA.GURU");
-        System.out.println("Another line");
+        System.out.println("Another line2");
     }
 }
