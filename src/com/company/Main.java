ccb1566997a5c70a13bf23553132dba5c80dffd6
Tatiana Terentyeva <tterentieva17@gmail.com>
2022-04-11 11:38:01 +0300

    switch to upper case

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,6 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello, qa.guru");
+        System.out.println("Hello, QA.GURU");
     }
 }
